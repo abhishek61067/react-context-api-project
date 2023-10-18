707d56e2684b95ae3352b70f7b877e8aa6a9ffdd
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2023-10-19 00:25:24 +0545

    feat: add counter to the cart navlink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import {useContext} from 'react'
+import {CartContext} from '../Context'
 
 const Header = () => {
+  const {cart,setCart} = useContext(CartContext)
   return (
     <div>
       <ul>
@@ -9,7 +12,7 @@ const Header = () => {
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/cart">Cart</Link>
+          <Link to="/cart">Cart <b>({cart.length})</b></Link>
         </li>
       </ul>
     </div>
